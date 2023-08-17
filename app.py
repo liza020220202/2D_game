5256a50e4e8965af6c72cc5733154a238fcb8040
Liza <elizaveta.markushkina@yandex.ru>
2023-08-17 10:42:22 +0300

    base

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
 import pygame as pg
+import sys
+
+
+def game():
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    clock = pg.time.Clock()
+
+    while True:
+        screen.fill((0, 0, 0))
+        pg.display.flip()
+
+        clock.tick(60) #FPS
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+
+WIDTH = 500
+HEIGHT = 400
+BACKGROUND = (0, 0, 0)
+
+if __name__ == '__main__':
+    game()
